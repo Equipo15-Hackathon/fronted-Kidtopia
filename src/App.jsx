0635227cbde63dd139d4166e037f0e59884c1d56
erikamc99 <erikamc99@gmail.com>
2025-03-19 00:30:52 +0100

    feat: change fake img to dont repeat file names

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import { CartProvider } from './context/CartContext'
 function App() {
   const products = [
     { name: "Muñeca", price: 10.99, image: "/img/doll.png" },
-    { name: "Carro", price: 15.50, image: "/img/cart.png" },
+    { name: "Carro", price: 15.50, image: "/img/cart-toy.png" },
     { name: "Coche", price: 19.99, image: "/img/car.png", }
 ];
 
   return (
 
     <CartProvider>
+      <Cart />          
       <div className="catalog-view">
         {products.map((product) => (
           <ProductCard key={product.name} {...product} />
         ))}
       </div>
-      <Cart />          
     </CartProvider>
   )
 }
