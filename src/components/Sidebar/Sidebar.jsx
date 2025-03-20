726d9fa62513e0088ddb781e0de36b7bc13dbac6
FrostyValue <saturmendezc@gmail.com>
2025-03-20 15:53:24 +0100

    feat:Sidebar totally integrated

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import { Icon } from "@iconify/react";
 import "./Sidebar.css";
 
-export default function Sidebar({open, setOpen}) {
-
-  //Data example
+export default function Sidebar({ open, setOpen, setSelectedCategory }) {
   const categories = [
     "Muñecas y Figuras de Acción",
     "Juegos de Construcción",
@@ -20,39 +17,21 @@ export default function Sidebar({open, setOpen}) {
     "Juguetes para Bebés",
   ];
 
-  // To close sidebar when clicking outside
-  const handleOutsideClick = (e) => {
-    if (open && !e.target.closest(".sidebar")) {
-      setOpen(false);
-    }
-  };
-
   return (
-    <div className="sidebar-container" onClick={handleOutsideClick}>
-      <button
-        className="toggle-button"
-        onClick={(e) => {
-          e.stopPropagation();
-          setOpen(!open);
-        }}
-      >
-        <Icon
-          icon={open ? "teenyicons:x-outline" : "teenyicons:menu-outline"}
-          width={24}
-          height={24}
-        />
-      </button>
-
-      <div
-        className={`sidebar ${open ? "open" : "closed"}`}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <h2 className="sidebar-title">Categorias</h2>
+    <div className={`sidebar-container ${open ? "open" : ""}`}>
+      <div className="sidebar">
+        <button className="toggle-button" onClick={() => setOpen(!open)}>
+          <Icon icon="teenyicons:x-outline" width={24} height={24} />
+        </button>
+        <h2 className="sidebar-title">Categorías</h2>
         <ul className="sidebar-list">
           {categories.map((category, index) => (
-            <li key={index} className="sidebar-item">
-              <Icon icon="teenyicons:box-outline" width={24} height={24} />{" "}
-              {category}
+            <li
+              key={index}
+              className="sidebar-item"
+              onClick={() => setSelectedCategory(category)}
+            >
+              <Icon icon="teenyicons:box-outline" width={24} height={24} /> {category}
             </li>
           ))}
         </ul>
