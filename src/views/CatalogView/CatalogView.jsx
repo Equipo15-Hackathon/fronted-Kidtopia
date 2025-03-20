01d4eb8dea58c9d1e83bc0d2377dcb1de54a8918
erikamc99 <erikamc99@gmail.com>
2025-03-20 13:56:29 +0100

    feat: sidebar filter functionality implemented

--- src/views/CatalogView/CatalogView.jsx
+++ src/views/CatalogView/CatalogView.jsx
@@ -7,34 +7,37 @@ import SideBar from '../../components/SideBar/SideBar';
 
 const CatalogView = () => {
   const [open, setOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
     const products = [
-        { name: "Muñeca", price: 10.99, image: "/img/doll.png", ageRange: "3-5 años", brand: "Mattel" },
-        { name: "Carro", price: 15.50, image: "/img/cart-toy.png", ageRange: "0-2 años", brand: "Hasbro" },
-        { name: "Coche", price: 19.99, image: "/img/car.png", ageRange: "6-8 años", brand: "Fisher-Price" },
-        { name: "Patata", price: 10.99, image: "/img/doll.png", ageRange: "9-12 años", brand: "Lego" },
-        { name: "Patata1", price: 15.50, image: "/img/cart-toy.png", ageRange: "0-2 años", brand: "Playmobil" },
-        { name: "Patata2", price: 19.99, image: "/img/car.png", ageRange: "6-8 años", brand: "Barbie" },
-        { name: "Patata3", price: 10.99, image: "/img/doll.png", ageRange: "3-5 años", brand: "Mattel" },
-        { name: "Patata4", price: 15.50, image: "/img/cart-toy.png", ageRange: "9-12 años", brand: "Hasbro" },
-        { name: "Patata5", price: 19.99, image: "/img/car.png", ageRange: "6-8 años", brand: "Playmobil" },
-        { name: "Patata6", price: 10.99, image: "/img/doll.png", ageRange: "3-5 años", brand: "Mattel" },
-        { name: "Patata7", price: 15.50, image: "/img/cart-toy.png", ageRange: "0-2 años", brand: "Hasbro" },
-        { name: "Patata8", price: 19.99, image: "/img/car.png", ageRange: "9-12 años", brand: "Fisher-Price" },
+        { category: "Muñecas y Figuras de Acción", name: "Muñeca", price: 10.99, image: "/img/doll.png", ageRange: "3-5 años", brand: "Mattel" },
+        { category: "Juegos de Mesa", name: "Carro", price: 15.50, image: "/img/cart-toy.png", ageRange: "0-2 años", brand: "Hasbro" },
+        { category: "Vehículos y Pistas", name: "Coche", price: 19.99, image: "/img/car.png", ageRange: "6-8 años", brand: "Fisher-Price" },
+        { category: "Juegos de Construcción", name: "Patata", price: 10.99, image: "/img/doll.png", ageRange: "9-12 años", brand: "Lego" },
+        { category: "Juguetes Educativos", name: "Patata1", price: 15.50, image: "/img/cart-toy.png", ageRange: "0-2 años", brand: "Playmobil" },
+        { category: "Juguetes Electrónicos", name: "Patata2", price: 19.99, image: "/img/car.png", ageRange: "6-8 años", brand: "Barbie" },
+        { category: "Peluches y Marionetas", name: "Patata3", price: 10.99, image: "/img/doll.png", ageRange: "3-5 años", brand: "Mattel" },
+        { category: "Arte y Manualidades", name: "Patata4", price: 15.50, image: "/img/cart-toy.png", ageRange: "9-12 años", brand: "Hasbro" },
+        { category: "Instrumentos Musicales", name: "Patata5", price: 19.99, image: "/img/car.png", ageRange: "6-8 años", brand: "Playmobil" },
+        { category: "Muñecas y Figuras de Acción", name: "Patata6", price: 10.99, image: "/img/doll.png", ageRange: "3-5 años", brand: "Mattel" },
+        { category: "Muñecas y Figuras de Acción", name: "Patata7", price: 15.50, image: "/img/cart-toy.png", ageRange: "0-2 años", brand: "Hasbro" },
+        { category: "Muñecas y Figuras de Acción", name: "Patata8", price: 19.99, image: "/img/car.png", ageRange: "9-12 años", brand: "Fisher-Price" },
 
     ];
 
   const { filteredProducts, filterProducts, resetFilters } = useFilters(products);
-  const categoryTitle = ["Juguetes"];
-  
+  const productsToDisplay = selectedCategory
+  ? filteredProducts.filter(product => product.category === selectedCategory)
+  : filteredProducts;
   return (
       <div className={`catalog-view ${open ? "sidebar-open" : ""}`}>
-        <SideBar open={open} setOpen={setOpen}/>
+        <SideBar open={open} setOpen={setOpen} setSelectedCategory={setSelectedCategory}/>
         <div className="catalog-container">
-          <h1 className="category-title">{categoryTitle}</h1>
+          <h1 className="category-title">{selectedCategory || "Todos"}</h1>
           <Filter filterProducts={filterProducts} resetFilters={resetFilters}/>
 
           <div className="products-container">
-            {filteredProducts.map((product) => (
+            {productsToDisplay.map((product) => (
               <ProductCard key={product.name} {...product} />
             ))}
           </div>
