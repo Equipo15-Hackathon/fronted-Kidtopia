d2ff1f7255bbe5bd30735719f59d7cff611e0bf5
FrostyValue <saturmendezc@gmail.com>
2025-03-20 21:17:18 +0100

    unnecesary code deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ContactView from './views/ContactView/ContactView.jsx'
-import TestApi from "./services/api/testApi.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TestApi />
   </StrictMode>,
 )
